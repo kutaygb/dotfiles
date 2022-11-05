7320e22649ae30cd8cb33a2cab0a79068544bfa3
kutaygb <kutaygb@protonmail.com>
2022-11-06 00:53:34 +0300

    s

--- .config/st/patch.sh
+++ .config/st/patch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+patch < patches/st-font2-0.8.5.diff
 patch < patches/st-blinking_cursor-20211116-2f6e597.diff
 patch < patches/st-scrollback-0.8.5.diff
 patch < patches/st-0.8.5-autocomplete-20220327-230120.diff
